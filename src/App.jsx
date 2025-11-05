f4dabb824bb0e09646740f0af5f62b806ec2a685
Gabriel Letto <158781940+DevLetto@users.noreply.github.com>
2025-11-05 15:50:43 -0300

    Commit Incial

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import Tasks from "./components/Tasks"
+import AddTask from "./components/AddTask";
+import { useEffect, useState } from "react";
+import {v4} from "uuid"
+import Title from "./components/Title";
+
+function App(){
+
+  const[tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  )
+
+  useEffect(() => {
+    
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+
+  }, [tasks] )
+
+  useEffect(() => {
+
+    async function fetchTasks() {
+      
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos/?_limit=10', {method: "GET"})
+
+      
+      const data = await response.json();
+      
+      const formattedTasks = data.map(task => ({
+      id: task.id,
+      text: task.title,          // a API usa "title"
+      description: "Sem descrição",
+      isCompleted: task.completed
+      }));
+
+      setTasks(formattedTasks);
+
+
+    }
+
+    // fetchTasks()
+
+  }, [])
+
+  function onAddTask(text, description){
+    const newTask = {
+      id: v4(),
+      text,
+      description,
+      isCompleted: false
+    }
+    setTasks([...tasks, newTask]);
+  }
+
+  function onKeyClick(event){
+    if(event.key == "Enter"){
+      onAddTask()
+    }
+  }
+
+  function onTaskClick(taskId){
+    const newTask = tasks.map(task => {
+
+      if(task.id == taskId){
+        return {...task, isCompleted: !task.isCompleted}
+      }
+      return task
+
+    })
+
+    setTasks(newTask)
+
+  }
+
+  function removeTask(taskId){
+    const newTask = tasks.filter(task => task.id !== taskId)
+
+    setTasks(newTask)
+  }
+
+  return(
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
+        <Title >Gerenciador de Tarefas</Title>
+      <AddTask onAddTask={onAddTask} onKeyClick ={onKeyClick}/>
+      <Tasks tasks={tasks} onTaskClick = {onTaskClick} removeTask = {removeTask} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
